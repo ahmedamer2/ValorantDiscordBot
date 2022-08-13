3fd7411a7537ae7e992f62caea99bd4d122d0b61
ahmedamer2 <63519817+ahmedamer2@users.noreply.github.com>
2022-08-12 19:01:38 -0600

    creat basic bot

---
+++ src/functions/handlers/handleCommands.js
@@ -0,0 +1,36 @@
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const fs = require("fs");
+
+module.exports = (client) => {
+	client.handleCommands = async () => {
+		const commandFolders = fs.readdirSync("./src/commands");
+		for (const folder of commandFolders) {
+			const commandFiles = fs
+				.readdirSync(`./src/commands/${folder}`)
+				.filter((file) => file.endsWith(".js"));
+
+			const { commands, commandArray } = client;
+			for (const file of commandFiles) {
+				const command = require(`../../commands/${folder}/${file}`);
+				commands.set(command.data.name, command);
+				commandArray.push(command.data.toJSON());
+				console.log(`Command: ${command.data.name} has passed through the handler`);
+			}
+		}
+
+		const clientId = "1007784350928089188";
+		const guildId = "916822558253412483";
+		const rest = new REST({ version: "9" }).setToken(process.env.BOT_TOKEN);
+
+		try {
+			console.log("Started refreshing application (/) commands");
+
+			await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+				body: client.commandArray
+			});
+		} catch (err) {
+			console.error(err);
+		}
+	};
+};
